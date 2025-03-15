304efa0a9566bd34617f5f07c71ec574e3897294
Shreya ⋆. 𐙚 ˚ <136884688+arceuzvx@users.noreply.github.com>
2025-03-15 19:09:09 +0530

    Update settings.py

--- entrack/settings.py
+++ entrack/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-=av9giby7kq!z1q=5-h3$y-md^jwg8#tp#ukr)*okamb92u_1a'
+SECRET_KEY = 'your_secret_key'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
